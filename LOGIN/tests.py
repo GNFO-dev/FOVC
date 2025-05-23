c9d76265fc8e156fb8bab4e8ab4c31150daa8683
GNFO-dev <gonzalo.flores.orellana@gmail.com>
2025-05-23 00:36:26 +0000

    MENSAJEE

--- LOGIN/tests.py
+++ LOGIN/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+"from django.test import TestCase
 
 # Create your tests here.
+"
+from django.test import TestCase
+
+class LoginTestCase(TestCase):
+    def test_login(self):
+        response = self.client.post('/login/', {'username': 'admin', 'password': '1234'})
+        self.assertEqual(response.status_code, 200)
